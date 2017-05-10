9f674895e192f328998db9d594f513e3da0a6328
junasmarts <junasmarts@gmail.com>
2017-05-11 01:55:07 +0800

    edit 3

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,6 @@ importScripts('sw-toolbox/sw-toolbox.js');
 //toolbox.options.cache.name = "hi-lombok-v1";
 
 var fileToCaches = [
-	'/site/index.html',
 	'index.html',
 	'index.html?homescreen=1',
 	'index.html?hs=1',
